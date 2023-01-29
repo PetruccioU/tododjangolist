7d97a44897813de7fd80bf5b3b2d84dc3db37970
Petr Utkin <billyrocketman@gmail.com>
2023-01-29 12:46:42 +0500

    new 'Category' table, to specify the task, which will be connected to 'TodoListItem' via Foreign Key.

--- todoproject/todoapp/models.py
+++ todoproject/todoapp/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-
+from django.urls import reverse
 # python manage.py sqlmigrate todoapp 0002
 class TodoListItem(models.Model):
     title = models.CharField(max_length=255)
@@ -10,12 +10,19 @@ class TodoListItem(models.Model):
     update_date = models.DateTimeField(auto_now=True)
     photo = models.ImageField(upload_to='photos/%Y/%m/%d')
     is_done = models.BooleanField(default=False)
+    cat= models.ForeignKey('Category', on_delete=models.PROTECT,null=True)
 
-# Create your models here.
+    def __str__(self):
+        return self.title
 
+# For dynamic URL use get_absolute_url.
+    def get_absolute_url(self):
+        return reverse('post', kwargs={'post_id': self.pk})
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
 
-
-
+    def __str__(self):
+        return self.name
 
 
